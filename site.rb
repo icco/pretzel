7e53de9137d9157f5fd00ad919c23011f0c324fb
Nat Welch <nat@natwelch.com>
2017-07-21 01:25:49 +0000

    Initial ideas

--- site.rb
+++ site.rb
@@ -4,9 +4,11 @@ require "sinatra"
 
 # https://www.twilio.com/docs/quickstart/ruby/sms/hello-monkey
 # https://www.twilio.com/docs/api/twiml/sms/twilio_request
-get "/sms-quickstart" do
+post "/sms" do
+  p params
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
-    r.message(body: "Hey Monkey. Thanks for the message!")
+    r.message(body: "Recorded!")
   end
+
   twiml.to_s
 end
